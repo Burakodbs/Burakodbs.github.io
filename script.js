52a729c422eac40ddf72afa14a629f9509c77438
burakodbs <burak.odabas2@ogr.sakarya.edu.tr>
2023-05-06 03:49:19 +0300

    gün5

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
-"use strict";
-  var formGiris = document.forms["iletisim"];  // form seçimi
-  var eposta = formGiris.eposta;   // eposta alanı seçimi
-  var sifre = formGiris["tel"];  // sifre alanı seçimi
-
-  formGiris.onsubmit = function() {
-    if (eposta.value != "" && sifre.value != "") {
-      alert(eposta.value);  // eposta alanı value özelliği
-      alert(sifre.value);   // sifre alanı value özelliği
-    } else {
-      alert("Tüm alanları doldurun !");
-      return false;  // form gönderimini iptal et
-    }
-  }
+function validateForm() {
+  var adsoyad = document.forms["contact"]["adsoyad"].value;
+  var cinsiyet = document.forms["contact"]["cinsiyet"].value;
+  var dg = document.forms["contact"]["dg"].value;
+  var renk = document.forms["contact"]["renk"].value;
+  var dosya = document.forms["contact"]["dosya"].value;
+  var sehir = document.forms["contact"]["sehir"].value;
+  var uni = document.forms["contact"]["uni"].value;
+  var amac1 = document.forms["contact"]["amac1"].value;
+  var amac2 = document.forms["contact"]["amac2"].value;
+  var amac3 = document.forms["contact"]["amac3"].value;
+  var tel = document.forms["contact"]["tel"].value;
+  var email = document.forms["contact"]["email"].value;
+  var konu = document.forms["contact"]["konu"].value;
+  var mesaj = document.forms["contact"]["mesaj"].value;
+  
+}
